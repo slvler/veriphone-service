ebd645ec2d6be237ebae757c03e5d3d6e68f7a57
slvler <56368718+slvler@users.noreply.github.com>
2023-11-04 16:48:45 +0300

    Update veriphone.php

--- config/veriphone.php
+++ config/veriphone.php
@@ -13,7 +13,7 @@ return [
     */
 
     'veriphone' => [
-        'base_url' => env('VERIPHONE_BASE_URLY'),
+        'base_url' => env('VERIPHONE_BASE_URL'),
         'api_key' => env('VERIPHONE_API_KEY'),
     ],
 ];
